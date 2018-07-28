01e865236ea80978bbe52e84119cca5ee0834242
alex-avila <alex.avilx@icloud.com>
2018-07-28 15:01:20 -0600

    Import Month comp into App

--- app/index.js
+++ app/index.js
@@ -9,6 +9,8 @@
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
 
+import Month from './screens/Month'
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -21,8 +23,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Month />
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     )
---
+++ app/screens/Month.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+type Props = {}
+export default class Month extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Months</Text>
+      </View>
+    )
+  }
+}
